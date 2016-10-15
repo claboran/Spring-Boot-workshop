9d68fd54ffdb62f384ad8662c1ca0253fcd73757
Christian Laboranowitsch <christian@laboranowitsch.de>
2016-10-15 11:50:42 +0200

    Using explicit configuration in Spring dependency management and injection of dependencies

--- exercise1/src/main/java/de/laboranowitsch/exercise1/ExternalServiceConfiguration.java
+++ exercise1/src/main/java/de/laboranowitsch/exercise1/ExternalServiceConfiguration.java
@@ -1,9 +1,13 @@
 package de.laboranowitsch.exercise1;
 
+import de.laboranowitsch.common.service.VatService;
 import de.laboranowitsch.common.sservice.CountryService;
 import de.laboranowitsch.common.sservice.CountryServiceImpl;
+import de.laboranowitsch.exercise1.service.CalculatorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 /**
  * Service Configuration for External Spring Beans.
@@ -13,8 +17,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ExternalServiceConfiguration {
 
+    /**
+     * External Dependency to an Bean what is injected.
+     *
+     * @param mapVatService
+     * @return {@link CountryService}
+     */
     @Bean
-    public CountryService countryService() {
-        return new CountryServiceImpl();
+    public CountryService countryService(Map<String, VatService> mapVatService) {
+        return new CountryServiceImpl(mapVatService.get("constVatService"));
     }
 }
--- workshop-common/src/main/java/de/laboranowitsch/common/sservice/CountryServiceImpl.java
+++ workshop-common/src/main/java/de/laboranowitsch/common/sservice/CountryServiceImpl.java
@@ -1,5 +1,7 @@
 package de.laboranowitsch.common.sservice;
 
+import de.laboranowitsch.common.service.VatService;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -8,6 +10,12 @@ import java.util.Locale;
  * Nothing gets Exposed at all
  */
 public class CountryServiceImpl implements CountryService {
+    private final VatService vatService;
+
+    public CountryServiceImpl(final VatService vatService) {
+        this.vatService = vatService;
+    }
+
     @Override
     public List<String> getIsoCodes() {
         return Arrays.asList(Locale.getISOCountries());
