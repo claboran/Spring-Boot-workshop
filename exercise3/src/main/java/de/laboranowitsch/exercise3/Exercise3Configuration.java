46806f9c0cc96e5afadbe2ef5a4332f771bc2a38
Christian Laboranowitsch <christian@laboranowitsch.de>
2016-10-19 07:39:50 +0200

    Swagger introduced

--- exercise3/src/main/java/de/laboranowitsch/exercise3/Exercise3Configuration.java
+++ exercise3/src/main/java/de/laboranowitsch/exercise3/Exercise3Configuration.java
@@ -2,9 +2,15 @@ package de.laboranowitsch.exercise3;
 
 import de.laboranowitsch.common.test.DbLoaderConfiguration;
 import de.laboranowitsch.common.test.Profiles;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * {@link org.springframework.context.annotation.Configuration} class
@@ -12,5 +18,15 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile(value = {Profiles.DEV_DERBY, Profiles.DEV_H2, Profiles.PROD})
 @Import(value = {DbLoaderConfiguration.class})
+@EnableSwagger2
 public class Exercise3Configuration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
